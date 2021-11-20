c7acea355756a2d16b8bede6b9241a4214ba3192
Eric Korth <ebkorth7@gmail.com>
2021-11-20 13:47:46 -0500

    Starting to build application with create-react-app. 11-19-2021

---
+++ src/components/CalculatorFrame.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class Calculator extends Component {
+
+
+    render() {
+        return (
+            <div className="container calculatorFrame">
+                <div className="row">
+                    <div className="col-md-6">
+                        {/* WILL PUT COMPONENTS OF THE CALCULATOR HERE */}
+                    </div>
+                </div>
+            </div>
+    );
+    }
+}
+
+export default Calculator;
